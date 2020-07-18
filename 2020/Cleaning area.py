dd66f4b4d0ec0810bd219a7e642f483743b6d27d
Developer-JG <h5638880@naver.com>
2020-07-18 22:40:56 +0900

    -

--- 2020/Cleaning area.py
+++ 2020/Cleaning area.py
@@ -1,5 +1,13 @@
 from random import randint
 from time import sleep
+import time
+
+name = 0
+try:
+    import name_file
+    name = 1
+except:
+    name = 0
 
 print("\n청소구역 배정 프로그램\n")
 
@@ -24,55 +32,6 @@ div_10 = division("교목실", 5, 0, [])
 div_list = [div_1, div_2, div_3, div_4, div_5, \
             div_6, div_7, div_8, div_9, div_10]
 
-def name(name):
-    if name == 1:
-        return '권준서'
-    elif name == 2:
-        return '김민성'
-    elif name == 3:
-        return '김하늘'
-    elif name == 4:
-        return '문세훈'
-    elif name == 5:
-        return '문채림'
-    elif name == 6:
-        return '박권영'
-    elif name == 7:
-        return '안종혁'
-    elif name == 8:
-        return '손유석'
-    elif name == 9:
-        return '윤호영'
-    elif name == 10:
-        return '이규민'
-    elif name == 11:
-        return '이시연'
-    elif name == 12:
-        return '이예나'
-    elif name == 13:
-        return '이은서'
-    elif name == 14:
-        return '전상민'
-    elif name == 15:
-        return '정재민'
-    elif name == 16:
-        return '정한솔'
-    elif name == 17:
-        return '조성현'
-    elif name == 18:
-        return '조용준'
-    elif name == 19:
-        return '진준'
-    elif name == 20:
-        return '진호영'
-    elif name == 21:
-        return '천건우'
-    elif name == 22:
-        return '최상민'
-    elif name == 23:
-        return '최수호'
-    elif name == 24:
-        return '황정규'
 
 def main():
     input("시작하려면 아무키나 누르세요.")
@@ -87,9 +46,9 @@ def main():
             if st_num >= 1:
                 break
             else:
-                print("1보다 큰 숫자를 입력하십시오\n")
+                print("0보다 큰 숫자를 입력하십시오\n")
         else:
-            print("유효한 숫자를 입력하십시오\n")
+            print("유효한 자연수를 입력하십시오\n")
 
     print()
 
@@ -102,7 +61,7 @@ def main():
             else:
                 print("시작번호보다 큰 숫자를 입력하십시오\n")
         else:
-            print("유효한 숫자를 입력하십시오\n")
+            print("유효한 자연수를 입력하십시오\n")
 
     print()
 
@@ -126,8 +85,22 @@ def main():
                         print("이미 제외된 숫자입니다.\n")
                 else:
                     print("숫자가 범위안에 있지 않습니다.\n")
+            else:
+                print("유효한 자연수를 입력하십시오.")
         else:
             break
+
+    print()
+
+    while True:
+        output_method = input("출력 방법을 선택하십시오. : ")
+        if output_method == 'name' or output_method == 'number':
+            if output_method == 'name' and name == 0:
+                print("'name_file'을 찾지 못하였습니다.")
+                output_method = 'number'
+            break
+        else:
+            print("유효한 출력 방법을 입력해주십시오.\n")
 
     print("\n\n")
     print("인식된 모든 번호 : ")
@@ -152,9 +125,10 @@ def main():
 
     sleep(2)
 
-    print("\n")
-    print("청소구역 역할분배를 시작합니다.")
-    print("\n")
+    print("\n"*30)
+    print("청소구역 역할분배를 시작합니다.\n")
+
+    start = time.time()
 
     while True:
         num = randint(0, len(all_list))
@@ -169,18 +143,29 @@ def main():
         if len(all_list) == 0:
             break
 
+    show_time = time.time() - start
+
     i = 0
     while i < len(div_list):
         sleep(0.4)
         print("{0} ({1} / {2})".format(div_list[i].name, div_list[i].now, div_list[i].max))
         div_list[i].list.sort()
-        for j in div_list[i].list:
-            print(name(j), end=' ')
-        print("\n")
-        i += 1
 
-    re = input("다시 시작하려면 'restrat'를 입력하십시오")
-    if re == "restrart":
+        if output_method == 'name':
+            for j in div_list[i].list:
+                print(name_file.name(j), end=' ')
+            print("\n")
+        elif output_method == 'number':
+            print(div_list[i].list)
+            print()
+
+        i += 1
+        sleep(0.2)
+
+    print("time : {0}\n".format(show_time))
+
+    re = input("다시 시작하려면 'restart'를 입력하십시오")
+    if re == "restart":
         main()
 
 if __name__ == "__main__":
