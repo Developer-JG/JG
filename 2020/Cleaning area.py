c9e973d4b2a1e85687c33db5074c01ddd25695d4
Developer-JG <h5638880@naver.com>
2020-07-30 22:33:21 +0900

    -

--- 2020/Cleaning area.py
+++ 2020/Cleaning area.py
@@ -1,12 +1,11 @@
 from random import randint
-from time import sleep
 import time
 import datetime
 
 name = 0
+
 try:
     from cleaning_area import name_file
-
     name = 1
 except:
     name = 0
@@ -22,6 +21,7 @@ class division:
         self.now = now
         self.list = []
 
+div_0 = division("(제외 인원)", 0, 0, [])
 div_1 = division("칠판 닦기", 2, 0, [])
 div_2 = division("교탁,사물함 닦기", 2, 0, [])
 div_3 = division("창문턱,거울 닦기", 1, 0, [])
@@ -33,15 +33,12 @@ div_8 = division("분리수거", 2, 0, [])
 div_9 = division("환경도우미", 1, 0, [])
 div_10 = division("교목실", 5, 0, [])
 
-div_list = [div_1, div_2, div_3, div_4, div_5, \
-            div_6, div_7, div_8, div_9, div_10]
+div_list = [div_0, div_1, div_2, div_3, div_4, div_5, div_6, div_7, div_8, div_9, div_10]
 
 
 def main():
     input("시작하려면 아무키나 누르세요.")
     print("\n")
-
-    sleep(0.5)
 
     while True:
         st_num = input("시작 번호를 입력하십시오. : ")
@@ -84,13 +81,60 @@ def main():
                 if st_num <= int(ex_num) <= ed_num:
                     try:
                         all_list.remove(int(ex_num))
-                        except_list.append(ex_num)
+                        except_list.append(int(ex_num))
+                        print("성공적으로 제외되었습니다.\n")
                     except:
                         print("이미 제외된 숫자입니다.\n")
                 else:
                     print("숫자가 범위안에 있지 않습니다.\n")
             else:
-                print("유효한 자연수를 입력하십시오.")
+                print("유효한 자연수를 입력하십시오.\n")
+        else:
+            break
+
+    print()
+
+    while True:
+        pu_num = input("추가할 번호를 입력하십시오. : ")
+        if pu_num != "":
+            if pu_num.isdigit():
+                if int(pu_num) not in all_list:
+                    all_list.append(int(pu_num))
+                    try:
+                        except_list.remove(int(ex_num))
+                    except:
+                        pass
+                    print("성공적으로 추가되었습니다.\n")
+                else:
+                    print("이미 추가된 숫자입니다.\n")
+            else:
+                print("유효한 자연수를 입력하십시오.\n")
+        else:
+            break
+
+    print()
+
+    while True:
+        in_num = input("특정부서에 넣을 번호를 입력하십시오. : ")
+        if in_num != '':
+            if in_num.isdigit():
+                if int(in_num) in all_list:
+                    in_div = input("해당번호를 넣을 부서를 선택하십시오. : ")
+                    if in_div.isdigit():
+                        in_div = int(in_div)
+                        if int(in_div) in range(len(div_list)):
+                            all_list.remove(int(in_num))
+                            div_list[in_div].list.append(int(in_num))
+                            div_list[in_div].now += 1
+                            print("성공적으로 추가되었습니다.\n")
+                        else:
+                            print("유효한 부서 번호를 입력하십시오.\n")
+                    else:
+                        print("유효한 자연수를 입력하십시오.\n")
+                else:
+                    print("이미 이동되었거나 숫자가 범위 안에 있지 않습니다.\n")
+            else:
+                print("유효한 자연수를 입력하십시오.\n")
         else:
             break
 
@@ -100,7 +144,7 @@ def main():
         output_method = input("출력 방법을 선택하십시오. : ")
         if output_method == 'name' or output_method == 'number':
             if output_method == 'name' and name == 0:
-                print("'name_file'을 찾지 못하였습니다.")
+                print("\n'name_file'을 찾지 못하였습니다.")
                 output_method = 'number'
             break
         else:
@@ -127,8 +171,6 @@ def main():
 
     input("(시작하려면 엔터)")
 
-    sleep(2)
-
     print("\n"*30)
     print("청소구역 역할분배를 시작합니다.\n")
 
@@ -145,7 +187,7 @@ def main():
     while True:
         num = randint(0, len(all_list))
         num = all_list[num - 1]
-        i = randint(0, len(div_list) - 1)
+        i = randint(1, len(div_list) - 1)
 
         if div_list[i].max > div_list[i].now:
             div_list[i].list.append(num)
@@ -158,28 +200,25 @@ def main():
 
     show_time = time.time() - start
 
-    i = 0
-    while i < len(div_list):
-        sleep(0.4)
-        print("{0} ({1} / {2})".format(div_list[i].name, div_list[i].now, div_list[i].max))
-        save_file.write("{0} ({1} / {2})".format(div_list[i].name, div_list[i].now, div_list[i].max) + "\n")
-        div_list[i].list.sort()
+    for i in range(len(div_list)):
+        if div_list[i].now > 0:
+            print("{0} ({1} / {2})".format(div_list[i].name, div_list[i].now, div_list[i].max))
+            save_file.write("{0} ({1} / {2})".format(div_list[i].name, div_list[i].now, div_list[i].max) + "\n")
+            div_list[i].list.sort()
 
-        if output_method == 'name':
-            for j in div_list[i].list:
-                print(name_file.name(j), end=' ')
-                save_file.write(str(name_file.name(j)) + " ")
-            print("\n")
-            save_file.write("\n\n")
+            if output_method == 'name':
+                for j in div_list[i].list:
+                    print(name_file.name(j), end=' ')
+                    save_file.write(str(name_file.name(j)) + " ")
+                print("\n")
+                save_file.write("\n\n")
 
-        elif output_method == 'number':
-            print(div_list[i].list)
-            save_file.write("{0}".format(div_list[i].list))
-            print()
-            save_file.write("\n\n")
+            elif output_method == 'number':
+                print(div_list[i].list)
+                save_file.write("{0}".format(div_list[i].list))
+                print()
+                save_file.write("\n\n")
 
-        i += 1
-        sleep(0.2)
 
     print("time : {0}\n".format(show_time))
 
