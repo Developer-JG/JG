86cb6dd137d2277d6da5f80159b3c05a8f3c79a1
Developer-JG <h5638880@naver.com>
2020-11-01 11:46:17 +0900

    -

---
+++ 2020/Fermat's Last Theorem.py
@@ -0,0 +1,24 @@
+def calculate_def(object_number, n):
+    print(object_number)
+    for i in range(1,object_number):
+
+        for j in range(1,object_number):
+
+            if i ** n + j ** n == object_number ** n:
+                print("{0} ** {1} + {2} ** {1} = {3} ** {1}".format(i, n, j, object_number))
+                input("success!")
+
+def main():
+    print("Fermat's Last Theorem\n")
+
+    n = 3
+
+    object_number = 3
+
+    while True:
+        calculate_def(object_number, n)
+
+        object_number += 1
+
+if __name__ == "__main__":
+    main()
