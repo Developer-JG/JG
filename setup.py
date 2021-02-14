482de758fddbf4f6fed920d0c1e3fe8cc8fa550b
jeonggyu hwang <h5638880@naver.com>
2021-02-14 21:50:30 +0900

    -

--- setup.py
+++ setup.py
@@ -1,76 +1,24 @@
-import sys
-import setuptools
-from setuptools.command.test import test as TestCommand
+from setuptools import setup, find_packages
 
+setup(
+    name             = 'JG',
+    version          = '1.0',
+    description      = 'All codes in Developer_JG',
+    author           = 'jeonggyu hwang',
+    author_email     = 'h5638880@naver.com',
+    url              = 'https://github.com/Developer-JG/JG',
+    download_url     = 'https://github.com/Developer-JG/JG.git',
+    install_requires = [ ],
+    packages         = find_packages(exclude = []),
+    python_requires  = '>=3',
+    classifiers      = [
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ]
+)
 
-
-class Tox(TestCommand):
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = ["-v", "-epy"]
-        self.test_suite = True
-
-    def run_tests(self):
-        #import here, cause outside the eggs aren't loaded
-        import tox
-        tox.cmdline(self.test_args)
-
-def has_environment_marker_support():
-    """
-    Tests that setuptools has support for PEP-426 environment marker support.
-    The first known release to support it is 0.7 (and the earliest on PyPI seems to be 0.7.2
-    so we're using that), see: http://pythonhosted.org/setuptools/history.html#id142
-    References:
-    * https://wheel.readthedocs.org/en/latest/index.html#defining-conditional-dependencies
-    * https://www.python.org/dev/peps/pep-0426/#environment-markers
-    """
-    import pkg_resources
-    try:
-        return pkg_resources.parse_version(setuptools.__version__) >= pkg_resources.parse_version('0.7.2')
-    except Exception as exc:
-        sys.stderr.write("Could not test setuptool's version: %s\n" % exc)
-        return False
-
-def main():
-    version = sys.version_info[:2]
-    install_requires = ['virtualenv>=1.11.2', 'py>=1.4.17', 'pluggy>=0.3.0,<0.4.0']
-    extras_require = {}
-    if has_environment_marker_support():
-        extras_require[':python_version=="2.6"'] = ['argparse']
-    else:
-        if version < (2, 7):
-            install_requires += ['argparse']
-    setuptools.setup(
-        name='tox-plus',
-        description='virtualenv-based automation of test activities',
-        long_description=open("README.md").read(),
-        url='https://github.com/Itxaka/tox-plus',
-        version='0.0.1',
-        license='http://opensource.org/licenses/MIT',
-        platforms=['unix', 'linux', 'osx', 'cygwin', 'win32'],
-        author='Itxaka serrano, Holger krekel',
-        author_email='itxakaserrano@gmail.com',
-        packages=['tox_plus'],
-        entry_points={'console_scripts': 'tox-plus=tox_plus:cmdline\ntox-plus-quickstart=tox_plus._quickstart:main'},
-        # we use a public tox version to test, see tox.ini's testenv
-        # "deps" definition for the required dependencies
-        tests_require=['tox-plus'],
-        cmdclass={"test": Tox},
-        install_requires=install_requires,
-        extras_require=extras_require,
-        classifiers=[
-            'Development Status :: 5 - Production/Stable',
-            'Intended Audience :: Developers',
-            'License :: OSI Approved :: MIT License',
-            'Operating System :: POSIX',
-            'Operating System :: Microsoft :: Windows',
-            'Operating System :: MacOS :: MacOS X',
-            'Topic :: Software Development :: Testing',
-            'Topic :: Software Development :: Libraries',
-            'Topic :: Utilities',
-            'Programming Language :: Python',
-            'Programming Language :: Python :: 3'],
-    )
-
-if __name__ == '__main__':
-    main()
+pip install setuptools
