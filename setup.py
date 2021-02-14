897461e7ae17537dac7daaaa8292a4cffcd8834c
jeonggyu hwang <h5638880@naver.com>
2021-02-14 21:08:31 +0900

    -

--- setup.py
+++ setup.py
@@ -1,18 +1,46 @@
-# -*- coding: utf-8 -*-
-import textwrap
-
+import io
 from setuptools import setup
 
+
+def make_long_description():
+    with io.open("README.md", encoding='UTF-8') as f:
+        readme = f.read()
+    with io.open("CHANGELOG", encoding='UTF-8') as f:
+        changelog = f.read()
+    return readme + "\n\n" + changelog
+
+
 setup(
-    use_scm_version={
-        "write_to": "src/tox/version.py",
-        "write_to_template": textwrap.dedent(
-            """
-             # coding: utf-8
-             from __future__ import unicode_literals
-             __version__ = {version!r}
-             """,
-        ).lstrip(),
+    name="JG",
+    description="distributing activities of the tox tool",
+    long_description=make_long_description(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Developer-JG/JG",
+    license="CC-BY-NC-SA",
+    platforms=["unix", "linux", "osx", "cygwin", "win32"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Framework :: tox",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS :: MacOS X",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Software Development :: Testing",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Utilities",
+        "Programming Language :: Python",
+    ],
+    packages=["detox"],
+    install_requires=["tox>=3.5,<3.7", "py>=1.4.27", "eventlet>=0.15.0"],
+    extras_require={"lint": ["black", "flake8"], "dev": ["pytest >= 3.8"]},
+    entry_points={
+        "console_scripts": "detox=detox.cli:main",
+        "tox": ["proclimit = detox.tox_proclimit"],
     },
-    package_dir={"": "src"},
 )
