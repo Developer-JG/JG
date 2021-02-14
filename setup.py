29f2d267886daa1c96620e49d1dae45da64aeae0
jeonggyu hwang <h5638880@naver.com>
2021-02-14 22:06:36 +0900

    -

--- setup.py
+++ setup.py
@@ -1,22 +1,47 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+
+about = {}
+with open('pur/__about__.py') as f:
+    exec(f.read(), about)
+
+packages = [
+    about['__title__'],
+]
+
+install_requires = [x.strip() for x in open('requirements.txt').readlines()]
 
 setup(
-    name             = 'JG',
-    version          = '1.0',
-    description      = 'All codes in Developer_JG',
-    author           = 'jeonggyu hwang',
-    author_email     = 'h5638880@naver.com',
-    url              = 'https://github.com/Developer-JG/JG',
-    download_url     = 'https://github.com/Developer-JG/JG.git',
-    install_requires = [ ],
-    packages         = find_packages(exclude = []),
-    python_requires  = '>=3',
-    classifiers      = [
+    name=about['__title__'],
+    version=about['__version__'],
+    license=about['__license__'],
+    description=about['__description__'],
+    long_description=open('README.rst').read(),
+    author=about['__author__'],
+    author_email=about['__author_email__'],
+    url=about['__url__'],
+    packages=packages,
+    package_dir={about['__title__']: about['__title__']},
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=install_requires,
+    entry_points={
+        'console_scripts': ['pur = pur.__init__:pur'],
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6'
-    ]
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Build Tools',
+    ],
 )
