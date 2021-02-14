6a91153fda6b35d11d6109a95f242942efcb7b6f
jeonggyu hwang <h5638880@naver.com>
2021-02-14 21:24:46 +0900

    -

--- setup.py
+++ setup.py
@@ -1,10 +1,6 @@
-import os
 from setuptools import setup
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
-setup(
-    name = "JG",
-    version = "0"
-)
+setup(name='JG',
+      version=0,
+      description="JG module",
+      )
