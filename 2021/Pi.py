6c10e6f49597024da38416c4cda132ded47c969b
Developer-JG <h5638880@naver.com>
2021-05-18 21:13:08 +0900

    -

--- 2021/Pi.py
+++ 2021/Pi.py
@@ -18,29 +18,41 @@ def prime_number(number):
     return True
 
 
-def Franciscus_Vieta(value):
-    value_1 = (2 ** (1 / 2))
+def Franciscus_Vieta(value, p_count):
+    count = 0
     try:
         while True:
-            value_1 += (2 + value) ** 1 / 2
-            value_2 = value_1 / 2
-            value *= value_2
-            print("{:.256g}".format(value * (1 / 2)))
+            count += 1
+            if value == 0:
+                value_1 = ((2 ** (1 / 2)) / 2)
+                value_2 = (2 ** (1 / 2))
+                value += value_1
+            else:
+                value_1 = (((2 + value_2) ** (1 / 2)) / 2)
+                value_2 = ((2 + value_2) ** (1 / 2))
+                value *= value_1
+            if count % p_count == 0:
+                print(value_1)
+                print("{0}회 시행 : {1:.256g}".format(count ,2 / value))
+                input()
     except:
         input("program is stoped")
         print("\n" * 2)
         main()
 
 
-def John_Wallis(value):
+def John_Wallis(value, p_count):
+    count = 0
     n = 1
     try:
         while True:
+            count += 1
             if value == 0:
                 value += ((2 * n) / ((2 * n) - 1)) * ((2 * n) / ((2 * n) + 1))
             else:
                 value *= ((2 * n) / ((2 * n) - 1)) * ((2 * n) / ((2 * n) + 1))
-            print("{:.256g}".format(value * 2))
+            if count % p_count == 0:
+                print("{0}회 시행 : {1:.256g}".format(count ,value * 2))
             n += 1
     except:
         input("program is stoped")
@@ -48,12 +60,15 @@ def John_Wallis(value):
         main()
 
 
-def James_Gregory_and_Leibniz(value):
+def James_Gregory_and_Leibniz(value, p_count):
+    count = 0
     n = 0
     try:
         while True:
+            count += 1
             value += ((-1) ** n) / ((2 * n) + 1)
-            print("{:.256g}".format(value * 4))
+            if count % p_count == 0:
+                print("{0}회 시행 : {1:.256g}".format(count ,value * 4))
             n += 1
     except:
         input("program is stoped")
@@ -61,13 +76,16 @@ def James_Gregory_and_Leibniz(value):
         main()
 
 
-def John_Machin(value):
+def John_Machin(value, p_count):
+    count = 0
     n = 1
     try:
         while True:
+            count += 1
             value += (((4 * ((-1) ** (n - 1))) / ((2 * n) - 1)) * ((1 / 5) ** ((2 * n) - 1))) - \
                      (((-1) ** (n - 1)) / ((2 * n) - 1) * (1 / 239))
-            print("{:.256g}".format((value * 4)))
+            if count % p_count == 0:
+                print("{0}회 시행 : {1:.256g}".format(count ,value * 4))
             n += 1
     except:
         input("program is stoped")
@@ -75,12 +93,15 @@ def John_Machin(value):
         main()
 
 
-def Euler_1(value):
+def Euler_1(value, p_count):
+    count = 0
     n = 1
     try:
         while True:
+            count += 1
             value += 1 / (n ** 2)
-            print("{:.256g}".format((value * 6) ** (1 / 2)))
+            if count % p_count == 0:
+                print("{0}회 시행 : {1:.256g}".format(count ,(value * 6) ** (1 / 2)))
             n += 1
     except:
         input("program is stoped")
@@ -88,12 +109,15 @@ def Euler_1(value):
         main()
 
 
-def Euler_2(value):
+def Euler_2(value, p_count):
+    count = 0
     n = 0
     try:
         while True:
+            count += 1
             value += 1 / ((1 + 2 * n) ** 2)
-            print("{:.256g}".format((value * 8) ** (1 / 2)))
+            if count % p_count == 0:
+                print("{0}회 시행 : {1:.256g}".format(count ,(value * 8) ** (1 / 2)))
             n += 1
     except:
         input("program is stoped")
@@ -101,12 +125,15 @@ def Euler_2(value):
         main()
 
 
-def Euler_3(value):
+def Euler_3(value, p_count):
+    count = 0
     n = 1
     try:
         while True:
+            count += 1
             value += (((-1) ** (n + 1)) / (n ** 2))
-            print("{:.256g}".format((value * 12) ** (1 / 2)))
+            if count % p_count == 0:
+                print("{0}회 시행 : {1:.256g}".format(count ,(value * 12) ** (1 / 2)))
             n += 1
     except:
         input("program is stoped")
@@ -114,16 +141,19 @@ def Euler_3(value):
         main()
 
 
-def Euler_prim(value):
+def Euler_prim(value, p_count):
+    count = 0
     n = 2
     try:
         while True:
             if prime_number(n) == True:
+                count += 1
                 if  value == 0:
                     value += ((n ** 2) / ((n ** 2) - 1))
                 else:
                     value *= ((n ** 2) / ((n ** 2) - 1))
-                print("{:.256g}".format(value))
+                if count % p_count == 0:
+                    print("{0}회 시행 : {1:.256g}".format(count ,(value * 6) ** (1 / 2)))
             n += 1
     except:
         input("program is stoped")
@@ -131,15 +161,18 @@ def Euler_prim(value):
         main()
 
 
-def Ramanujan(value):
+def Ramanujan(value, p_count):
+    count = 0
     n = 0
     try:
         while True:
+            count += 1
             value_1 = factoriacl(4 * n) / factoriacl(n)
             value_2 = ((26390 * n) + 1103) / (396 ** (4 * n))
             value_0 = value_1 * value_2
             value += value_0
-            print("{:.256g}".format(((2 * (2 ** (1 / 2))) / (99 ** 2) * value) ** -1))
+            if count % p_count == 0:
+                print("{0}회 시행 : {1:.256g}".format(count ,((2 * (2 ** (1 / 2))) / (99 ** 2) * value) ** -1))
             n += 1
     except:
         input("program is stoped")
@@ -148,26 +181,29 @@ def Ramanujan(value):
 
 
 def main():
+    count = 1
     value = 0
     while True:
         sel = input(" : ")
 
-        if sel == 'John_Wallis':
-            John_Wallis(value)
+        if sel == 'Franciscus_Vieta':
+            Franciscus_Vieta(value, count)
+        elif sel == 'John_Wallis':
+            John_Wallis(value, count)
         elif sel == 'James_Gregory_and_Leibniz':
-            James_Gregory_and_Leibniz(value)
+            James_Gregory_and_Leibniz(value, count)
         elif sel == 'John_Machin':
-            John_Machin(value)
+            John_Machin(value, count)
         elif sel == 'Euler_1':
-            Euler_1(value)
+            Euler_1(value, count)
         if sel == 'Euler_2':
-            Euler_2(value)
+            Euler_2(value, count)
         elif sel == 'Euler_3':
-            Euler_3(value)
+            Euler_3(value, count)
         elif sel == 'Euler_prim':
-            Euler_prim(value)
+            Euler_prim(value, count)
         elif sel == 'Ramanujan':
-            Ramanujan(value)
+            Ramanujan(value, count)
 
 
 if __name__ == "__main__":
